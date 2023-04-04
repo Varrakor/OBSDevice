1ad780e9b78743a56c409d8e377a90189855db31
Jasper Paterson <jasperpato@gmail.com>
2023-04-04 11:34:42 +0800

    ppt-switch

--- obs-scene-switcher/ppt.py
+++ obs-scene-switcher/ppt.py
@@ -3,10 +3,11 @@ import subprocess
 UP=126
 DOWN=125
 
-script="""tell application "Microsoft PowerPoint"
+# simple arrow presses
+script='''tell application "Microsoft PowerPoint"
   activate
   tell app "System Events" to key code {}
-end tell"""
+end tell'''
 
 def ppt(key):
   if key == 'prev': lines = script.format(UP).split('\n')
@@ -14,4 +15,10 @@ def ppt(key):
   if key in 'prevnext':
     cmd = 'osascript'
     for l in lines: cmd += f' -e \'{l}\''
-    subprocess.run(cmd, shell=True, capture_output=True)
+    subprocess.run(cmd, shell=True)
+
+if __name__ == '__main__':
+  while True:
+    key = input().strip()
+    if key in 'wa': ppt('prev')
+    else: ppt('next')
