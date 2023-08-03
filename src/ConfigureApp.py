33740359186e141fea916a61d470b569f0dbc05b
TheRyuBlitz <brandonke2000@gmail.com>
2023-08-03 15:18:03 +0800

    Update + fix Configure App for GUI

--- src/ConfigureApp.py
+++ src/ConfigureApp.py
@@ -8,7 +8,6 @@ class ConfigureApp():
     def __init__(self):
         # Base set up of configuration app that will connect to the GUI
         self.master = tk.Tk()
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing_app)
         env = dotenv_values()
 
         # Set up attribute for App to store a GUI object
@@ -42,10 +41,6 @@ class ConfigureApp():
         self.appButton = tk.Button(self.master, text='Start App', command=self.start_app)
         self.appButton.grid(column=0, row=4, padx=10, pady=20)
 
-        # Set up button that will stop the currently running GUI
-        self.stopButton = tk.Button(self.master, text='Stop App', command=self.stop_app)
-        self.stopButton.config(state = tk.DISABLED)
-        self.stopButton.grid(column=1, row=4, padx=10, pady=20)
     
     def start_app(self):
         """
@@ -53,46 +48,40 @@ class ConfigureApp():
         @author:    Brandon
         """
         # These lines may not be necessary when not testing with GUI
-        self.appButton.config(state = tk.DISABLED)
-        self.stopButton.config(state = tk.NORMAL)
-        self.gui = GUI(self.password.get(), port=self.port.get(), host=self.host.get())
-        self.gui.master.protocol("WM_DELETE_WINDOW", self.on_closing_gui)
-    
-    def stop_app(self):
-        """
-        @summary:   Stop the app of the video switcher. If the GUI is not currently running, disable it. Otherwise, have it enabled.
-        @author:    Brandon
-        """
-        self.appButton.config(state = tk.NORMAL)
-        self.stopButton.config(state = tk.DISABLED)
-        self.gui.force_close()
-        self.gui = ''
-
-    
-    def on_closing_gui(self):
-        """
-        @summary:   If the termination (close window) of the GUI is detected, enable the button on the ConfigureApp that will start the GUI, and disable the stop button.
-        @author:    Brandon
-        """
-        self.gui.force_close()
-        self.gui = ''
-        if self.appButton["state"] == tk.DISABLED:
-            self.appButton.config(state = tk.NORMAL)
-        if self.stopButton["state"] == tk.NORMAL:
-            self.stopButton.config(state = tk.DISABLED)
-    
-    def on_closing_app(self):
-        """
-        @summary:   When ConfigureApp itself closes, also close the GUI connected to it.
-        @author:    Brandon
-        """
-        if self.gui:
-            self.gui.force_close()
-        self.master.destroy()
+        self.master.quit()
 
     def loop(self):
         self.master.mainloop()
 
 
+def process():
+    """
+    @summary:   Function to continuously run Configuration App to set up GUI for OBS Video Switcher
+    @author:    Brandon
+    """
+    while True:
+        app = ConfigureApp()
+        app.loop()
+
+        try:
+            password = app.password.get()
+            port = app.port.get()
+            host = app.host.get()
+
+            params = [password, port, host]
+            
+            if any(not param for param in params):
+                print('NOT ENOUGH PARAMETERS WERE PROVIDED TO CONNECT TO THE OBS! TRY AGAIN...')
+                app.master.destroy()
+                continue
+            
+            gui = GUI(password, port=port, host=host)
+            app.master.destroy()
+            gui.loop()
+        except:
+            # If the Configuration App was closed by the user, close entier process
+            break
+
+
 if __name__ == '__main__':
-    ConfigureApp().loop()
+    process()
